fcbe6ab2266f1c8e454f7f941093c4f2cedd7ec2
dsl2055 <job2019tmm@gmail.com>
2022-06-18 09:47:50 -0400

    first commit

---
+++ countTrain.js
@@ -0,0 +1,10 @@
+let newInventoryCarModels = ["S1", "T1", "S2", "S1", "S2", "S1"];
+let newInventoryCarTypes = [
+  "StockCar",
+  "Refrigerator",
+  "StockCar",
+  "Lorrie",
+  "Lorrie",
+  "Lorrie",
+];
+let newInventoryYearBuilt = [1922, 2011, 1975, 2002, 1965, 1951];
